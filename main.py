9ef574ba531c0925a4f6885d2aa64b31865434b6
Milan Singh <33810490+Milansingh163@users.noreply.github.com>
2023-03-20 12:13:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from flask import Flask, jsonify
 import os
+from flask import request
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+@app.route('/home')
+def hello_world():
+    return 'This is my home page'
 
+@app.route('/name')
+def get_name():
+    data = request.args.get('val')
+    return 'my name is {}'.format(data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
